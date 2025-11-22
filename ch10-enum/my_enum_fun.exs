1e240ca0470f0c94e271de93ae0dde0b61c2a32a
Nicholas Stevens <nichstev@gmail.com>
2025-11-22 21:08:16 +0100

    Finish MyEnumFun implemetation with takefunction

--- ch10-enum/my_enum_fun.exs
+++ ch10-enum/my_enum_fun.exs
@@ -41,6 +41,22 @@ defmodule MyEnumFun do
     reverse(tail, [ head | acc ])
   end
 
-  # take -  not yet implemented
+  # take
+  def take(list, num) when num < 0 do 
+    taker(reverse(list), -num, [], :neg)
+  end
 
+  def take(list, num) do 
+    taker(list, num, [], :pos)
+  end
+
+  defp taker([], _num, acc, :pos), do: reverse(acc)
+  defp taker([], _num, acc, :neg), do: acc
+
+  defp taker(_, 0, acc, :pos), do: reverse(acc)
+  defp taker(_, 0, acc, :neg), do: acc
+
+  defp taker([head | tail], num, acc, flag) do
+    taker(tail, num - 1, [head | acc], flag)
+  end
 end
