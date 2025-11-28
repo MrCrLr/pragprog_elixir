3fc2139fac7cb66a98a2a3f75c2f1ffcbe0a6078
Nicholas Stevens <nichstev@gmail.com>
2025-11-28 07:26:05 +0100

    Add functionality. Receive charlist or binary or any combination thereof

--- ch11-strings/anagram_check.exs
+++ ch11-strings/anagram_check.exs
@@ -1,10 +1,33 @@
 defmodule AnagramCheck do
-  # Takes 2 charlists
+  # Takes two charlists or binaries or any combination
+  def anagram?(word1, word2) 
+  when is_binary(word1) and is_binary(word2) do
+    a = String.to_charlist(word1)
+    b = String.to_charlist(word2)
+    anagram?(a, b)
+  end
+  
+  def anagram?(word1, word2) 
+  when is_binary(word1) and is_list(word2) do
+    a = String.to_charlist(word1)
+    b = word2  
+    anagram?(a, b)
+  end
+
+  def anagram?(word1, word2) 
+  when is_list(word1) and is_binary(word2) do
+    a = word1
+    b = String.to_charlist(word2)
+    anagram?(a, b)
+  end  
+    
   def anagram?(word1, word2) do
     a = word1 -- word2
     b = word2 -- word1
     _anagram?(a, b)
   end
+
   defp _anagram?([], []), do: true
   defp _anagram?(_a, _b), do: false
 end
+
