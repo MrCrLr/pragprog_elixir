16e625c27c08d0ddc7641addefd3405e35a7b3ed
Nicholas Stevens <nichstev@gmail.com>
2025-11-30 07:07:43 +0100

    Update File.open call

--- ch11-strings/parse_csv.exs
+++ ch11-strings/parse_csv.exs
@@ -1,12 +1,18 @@
 defmodule CSVParser do
+import String, 
+  only: [split: 2, to_atom: 1, to_float: 1, 
+         to_integer: 1, trim: 1, trim_leading: 2]
+import Float, 
+  only: [round: 2]
+
   def parse(file) do
-    { :ok, f } = File.open(file)
+    f = File.open!(file)
     keys = 
       f 
       |> IO.read(:line)
-      |> String.trim()
-      |> String.split(",")
-      |> Enum.map(&String.to_atom/1)    
+      |> trim()
+      |> split(",")
+      |> Enum.map(&to_atom/1)    
     rows = 
       IO.stream(f, :line)
       |> Enum.map(&parse_row(&1, keys))
@@ -17,18 +23,15 @@ defmodule CSVParser do
   defp parse_row(line, keys) do
     values =
       line
-      |> String.trim()
-      |> String.split(",")
+      |> trim()
+      |> split(",")
     keys
     |> Enum.zip(values)
     |> Enum.map(&convert/1)
   end
 
-  defp convert({:id, v}), do: {:id, String.to_integer(v)}
-  defp convert({:ship_to, v}) do 
-    cleaned = String.trim_leading(v, ":")
-    {:ship_to, String.to_atom(cleaned)}
-  end
-  defp convert({:net_amount, v}), do: {:net_amount, Float.round(String.to_float(v), 2)}
+  defp convert({:id, v}),         do: {:id, to_integer(v)}
+  defp convert({:ship_to, v}),    do: {:ship_to, v |> trim_leading(":") |> to_atom()}
+  defp convert({:net_amount, v}), do: {:net_amount, v |> to_float() |> round(2)}
 end    
 
